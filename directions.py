a8d7cc6cc2ac082a5a6263285e77a39bfebbac9a
rakan19-meet <rakan19@meet.mit.edu>
2017-07-31 15:35:16 +0300

    Stopping here

---
+++ directions.py
@@ -0,0 +1,10 @@
+up_arrow="up"
+left_arrow="left"
+down_arrow="down"
+right_arrow="right"
+spacebar="space"
+
+
+up=0
+
+direction=up
---
+++ python_turtle_direction.py
@@ -0,0 +1,51 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+Traceback (most recent call last):
+  File "/home/student/rakan19_lab6/meet2017y1lab6/funturtle.py", line 6, in <module>
+    square.pendown(50,50)
+TypeError: pendown() takes 1 positional argument but 3 were given
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
+====== RESTART: /home/student/rakan19_lab6/meet2017y1lab6/funturtle.py ======
+>>> 
